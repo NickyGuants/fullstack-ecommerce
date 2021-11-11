d12c3e0a8ff49f7f6c3506b5625933d421ea71a5
NickyGuants <nicholasguantai528@gmail.com>
2021-11-11 16:02:49 +0300

    display products by fetching from db using axios

---
+++ client/src/components/Products.js
@@ -0,0 +1,35 @@
+import React, { useState, useEffect } from 'react';
+import { Link } from 'react-router-dom'
+import axios from 'axios'
+
+const Products = () => {
+    const [products, setProducts] = useState([]);
+    
+    useEffect(() => {
+        const fetchProducts = async () => {
+            const { data } = await axios.get('api/products');
+            setProducts(data);
+        }
+
+        fetchProducts();
+    }, [])
+
+    return (
+        <section className="container">
+            <div className="products">
+                {products.map((vehicle) => (
+                    <div key={vehicle.id} className="card">
+                        <Link to={`details/${vehicle.id}`}>
+                            <img src={vehicle.imgSrc} alt={vehicle.name} />
+                        </Link>
+                        <p>Model: {vehicle.model}</p>
+                        <p>Make: {vehicle.make}</p>
+                        <p>Price: ${ vehicle.price.toLocaleString()}</p>
+                    </div>
+                ))}
+            </div>
+        </section>
+    )
+}
+
+export default Products;
