7d457be30faf8cdd830c621fe3585794a281fdbf
NickyGuants <nicholasguantai528@gmail.com>
2021-11-11 16:01:53 +0300

    create header component

---
+++ client/src/components/Header.js
@@ -0,0 +1,26 @@
+import React from "react";
+import '../App.css';
+import { Link } from 'react-router-dom'
+//import Search from "./Search";
+import { AiOutlineShoppingCart } from 'react-icons/ai';
+//import { useSelector } from 'react-redux';
+
+
+const Header = () => {
+    //const  cartItems  = useSelector((state) => state.cart.cartItems)
+    return(
+        <div>
+            <header>
+                <div>
+                    <h1>Pearl Shine Motors</h1>
+                </div>
+                <nav>  
+                    <Link to="/">HOME</Link> 
+                    <Link to="/cart" className="cart-link"><AiOutlineShoppingCart id="cart-icon" /><span>{ 0 }</span></Link>
+                </nav>
+            </header>
+        </div>
+    )
+}
+
+export default Header;
