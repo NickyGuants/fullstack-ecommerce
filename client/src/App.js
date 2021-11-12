7347478850e766f4e6ede687b5c4118d5b998544
NickyGuants <nicholasguantai528@gmail.com>
2021-11-12 06:53:51 +0300

    add details page to the app component as a route

--- client/src/App.js
+++ client/src/App.js
@@ -3,6 +3,7 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import Header from './components/Header';
 import Footer from './components/Footer';
 import Products from './components/Products';
+import ProductDetails from './components/ProductDetails';
 
 function App() {
   return (
@@ -10,7 +11,8 @@ function App() {
       <div className="App">
         <Header />
         <Routes>
-          <Route path="/" element={ <Products />}/>
+          <Route path="/" element={<Products />} />
+          <Route path="/:id" element={ <ProductDetails />}/>
         </Routes>  
       <Footer />
       </div>
