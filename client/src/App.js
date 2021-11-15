cb2a02aee0204ab9dd11e6bfcd02621cdae247c8
NickyGuants <nicholasguantai528@gmail.com>
2021-11-15 12:22:48 +0300

    add home to app

--- client/src/App.js
+++ client/src/App.js
@@ -2,9 +2,9 @@ import './App.css';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import Header from './components/Header';
 import Footer from './components/Footer';
-import Products from './components/Products';
 import ProductDetails from './components/ProductDetails';
 import Cart from './components/Cart'
+import Home from './components/Home';
 
 function App() {
   return (
@@ -12,7 +12,7 @@ function App() {
       <div className="App">
         <Header />
         <Routes>
-          <Route path="/" element={<Products />} />
+          <Route path="/" element={<Home />} />
           <Route path="/:id" element={<ProductDetails />} />
           <Route path="/cart" element={<Cart />} />
         </Routes>  
