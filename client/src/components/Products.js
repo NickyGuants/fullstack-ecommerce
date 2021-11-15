3ef2c2fd872ee6b698fdc60ff2cacd14e58e87de
NickyGuants <nicholasguantai528@gmail.com>
2021-11-15 06:09:25 +0300

    dispatch action to list the products

--- client/src/components/Products.js
+++ client/src/components/Products.js
@@ -1,33 +1,37 @@
-import React, { useState, useEffect } from 'react';
+import React, { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux'
 import { Link } from 'react-router-dom'
-import axios from 'axios'
+import { listProducts } from '../redux/actions/productActions';
 
 const Products = () => {
-    const [products, setProducts] = useState([]);
+    const dispatch = useDispatch();
+    const productList = useSelector(state => state.productList)
+    const { loading, error, products } = productList;
     
     useEffect(() => {
-        const fetchProducts = async () => {
-            const { data } = await axios.get('api/products');
-            setProducts(data);
-        }
-
-        fetchProducts();
-    }, [])
+        dispatch(listProducts())
+    }, [dispatch])
 
     return (
         <section className="container">
+            {loading ? (
+                <h2>Loading</h2>
+            ) : error ? (
+                    <h2>{ error }</h2>
+            ) :
             <div className="products">
-                {products.map((vehicle) => (
-                    <div key={vehicle.id} className="card">
-                        <Link to={`${vehicle.id}`}>
-                            <img src={vehicle.imgSrc} alt={vehicle.name} />
-                        </Link>
-                        <p>Model: {vehicle.model}</p>
-                        <p>Make: {vehicle.make}</p>
-                        <p>Price: ${ vehicle.price.toLocaleString()}</p>
-                    </div>
-                ))}
-            </div>
+            {products.map((vehicle) => (
+                <div key={vehicle.id} className="card">
+                    <Link to={`${vehicle.id}`}>
+                        <img src={vehicle.imgSrc} alt={vehicle.name} />
+                    </Link>
+                    <p>Model: {vehicle.model}</p>
+                    <p>Make: {vehicle.make}</p>
+                    <p>Price: ${ vehicle.price.toLocaleString()}</p>
+                </div>
+            ))}
+        </div>}
+            
         </section>
     )
 }
