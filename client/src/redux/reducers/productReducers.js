b176d2936b9dec6ebb6d3abe3c6fce46e4bcd1d2
NickyGuants <nicholasguantai528@gmail.com>
2021-11-15 06:44:11 +0300

    create product details reducer

--- client/src/redux/reducers/productReducers.js
+++ client/src/redux/reducers/productReducers.js
@@ -14,5 +14,17 @@ export const productListReducer = (state = { products: [] }, action) => {
     }
 }
 
-
+//product details reducer
+export const productDetailsReducer = (state = { product: { } }, action) => {
+    switch (action.type) {
+        case PRODUCT_DETAILS_REQUEST:
+            return { loading: true, ...state }
+        case PRODUCT_DETAILS_SUCCESS:
+            return { loading: false, product: action.payload }
+        case PRODUCT_DETAILS_FAIL:
+            return { loading: false, error: action.payload }
+        default:
+            return state
+    }
+}
 
