e481f910f6695ebdff52bb952abafec0990ae0b4
NickyGuants <nicholasguantai528@gmail.com>
2021-11-15 06:45:11 +0300

    create the action to fetch single product from db

--- client/src/redux/actions/productActions.js
+++ client/src/redux/actions/productActions.js
@@ -1,6 +1,7 @@
 import axios from 'axios'
-import { PRODUCT_LIST_REQUEST, PRODUCT_LIST_SUCCESS, PRODUCT_LIST_FAIL } from '../types'
+import { PRODUCT_LIST_REQUEST, PRODUCT_LIST_SUCCESS, PRODUCT_LIST_FAIL, PRODUCT_DETAILS_REQUEST, PRODUCT_DETAILS_SUCCESS, PRODUCT_DETAILS_FAIL } from '../types'
 
+//action to fetch list of products
 export const listProducts = () => async (dispatch) => {
     try {
         dispatch({ type: PRODUCT_LIST_REQUEST });
@@ -12,6 +13,24 @@ export const listProducts = () => async (dispatch) => {
     } catch (error) {
         dispatch({
             type: PRODUCT_LIST_FAIL,
+            payload: error.response && error.response.data.message ? error.response.data.message: error.message
+        })
+    }
+    
+}
+
+//action to fetch a single product using an id
+export const listProductDetails = (id) => async (dispatch) => {
+    try {
+        dispatch({ type: PRODUCT_DETAILS_REQUEST });
+        const { data } = await axios.get(`/api/products/${id}`);
+        dispatch({
+            type: PRODUCT_DETAILS_SUCCESS,
+            payload: data
+        })
+    } catch (error) {
+        dispatch({
+            type: PRODUCT_DETAILS_FAIL,
             payload: error.response && error.response.data.message ? error.response.data.message: error.message
         })
     }
