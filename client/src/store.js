99655ec7496ff3dd42538f49d0fc92f884fe29c1
NickyGuants <nicholasguantai528@gmail.com>
2021-11-15 09:28:08 +0300

    add cart reducer to the store

--- client/src/store.js
+++ client/src/store.js
@@ -2,10 +2,12 @@ import { createStore, combineReducers, applyMiddleware } from 'redux'
 import thunk from 'redux-thunk';
 import { composeWithDevTools } from 'redux-devtools-extension';
 import { productListReducer, productDetailsReducer } from './redux/reducers/productReducers'
+import cartReducer from './redux/reducers/cartReducers';
 
 const reducers = combineReducers({
     productList: productListReducer,
-    productDetails: productDetailsReducer
+    productDetails: productDetailsReducer,
+    cart: cartReducer
 
 });
 const middleware = [thunk];
