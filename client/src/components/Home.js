b8d9cfd301c13848902c4e4484128f37d7abca3f
NickyGuants <nicholasguantai528@gmail.com>
2021-11-15 12:22:23 +0300

    add home page

---
+++ client/src/components/Home.js
@@ -0,0 +1,15 @@
+import React from "react";
+import Hero from "./Hero";
+import Products from "./Products";
+
+
+const Home = () => {
+    return (
+        <div className="home">
+            <Hero />
+            <Products /> 
+        </div>
+    )
+}
+
+export default Home;
