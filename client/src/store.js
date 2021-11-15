dd2ca9794a98cb8bca8e9575d8bf495aad26b795
NickyGuants <nicholasguantai528@gmail.com>
2021-11-15 06:44:28 +0300

    add product details reducer to the store

--- client/src/store.js
+++ client/src/store.js
@@ -1,10 +1,12 @@
 import { createStore, combineReducers, applyMiddleware } from 'redux'
 import thunk from 'redux-thunk';
 import { composeWithDevTools } from 'redux-devtools-extension';
-import { productListReducer } from './redux/reducers/productReducers'
+import { productListReducer, productDetailsReducer } from './redux/reducers/productReducers'
 
 const reducers = combineReducers({
-    productList: productListReducer
+    productList: productListReducer,
+    productDetails: productDetailsReducer
+
 });
 const middleware = [thunk];
 const initialState = {};
