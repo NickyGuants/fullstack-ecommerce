96f7b1b8489f6fafb33006d0b10aadd1f4c89787
NickyGuants <nicholasguantai528@gmail.com>
2021-11-16 14:17:19 +0300

    add register reducer to the store

--- client/src/store.js
+++ client/src/store.js
@@ -3,13 +3,14 @@ import thunk from 'redux-thunk';
 import { composeWithDevTools } from 'redux-devtools-extension';
 import { productListReducer, productDetailsReducer } from './redux/reducers/productReducers'
 import cartReducer from './redux/reducers/cartReducers';
-import { userLoginReducer } from './redux/reducers/userReducers'
+import { userLoginReducer, userRegisterReducer } from './redux/reducers/userReducers'
 
 const reducers = combineReducers({
     productList: productListReducer,
     productDetails: productDetailsReducer,
     cart: cartReducer,
-    userLogin: userLoginReducer
+    userLogin: userLoginReducer,
+    userRegister: userRegisterReducer
 });
 const middleware = [thunk];
 const initialState = {};
